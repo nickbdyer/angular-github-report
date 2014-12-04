42a07faada1394a234c2cdb1fe5d7aa5e8a27f1a
1278dale <dalestevens1278@gmail.com>
2014-12-04 14:22:37 +0000

    Added user input function to search

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,8 +2,8 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
 var searchResource = $resource('https://api.github.com/search/users');
   $scope.doSearch = function() {
     $scope.searchResult = searchResource.get({
-      q: $scope.searchTerm
+      q: $scope.searchTerm,
+      access_token: "5d6ffa56fba974adfd2dc6b86157d542c67256c0",
     });
-    $scope.$apply();
   };
 });
