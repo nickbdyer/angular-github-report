0c3cffc61467a85a794299f33bbe952c82044a53
Nick Dyer <nbdyer@gmail.com>
2014-12-05 08:08:38 +0000

    testing with scope

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,14 +1,15 @@
 githubUserSearch.controller('GitUserSearchController', function($scope, $resource) {
 var searchResource = $resource('https://api.github.com/search/users');
     $scope.searchHistory = [];
+
   $scope.doSearch = function() {
     if ($scope.searchTerm !== ""){
     $scope.searchResult = searchResource.get({
       q: $scope.searchTerm,
       access_token: "5d6ffa56fba974adfd2dc6b86157d542c67256c0",
-    });
+      });
+    };
   };
-};
 
   $scope.addSearchTerm = function() {
     $scope.searchHistory.push($scope.searchTerm);
@@ -18,4 +19,10 @@ var searchResource = $resource('https://api.github.com/search/users');
     return searchHistory[searchHistory.length-1]
   };
 
+  $scope.showUser = function(username) {
+      var userResource = $resource('https://api.github.com/users/' + username + '/repos');
+      $scope.userRepoResult = userResource.query({
+        access_token: "5d6ffa56fba974adfd2dc6b86157d542c67256c0"
+      })
+    };
 });
